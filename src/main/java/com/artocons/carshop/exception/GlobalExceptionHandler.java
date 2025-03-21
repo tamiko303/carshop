c238992859a5911f94b8705a92ed321853e8e7f2
Tatsiana Kastrubaiu <tatsiana.kastrubai@artocons.com>
2025-03-21 10:29:36 +0300

    feat: implement exception handler

--- src/main/java/com/artocons/carshop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/artocons/carshop/exception/GlobalExceptionHandler.java
@@ -10,11 +10,11 @@ import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-//    @ExceptionHandler(ResourceNotFoundException.class)
-//    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-//        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-//    }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> globleExcpetionHandler(Exception ex, WebRequest request) {
