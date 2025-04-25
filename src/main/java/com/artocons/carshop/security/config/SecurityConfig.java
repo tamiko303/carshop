aabdf1a90d890b7db7cd225abf199fdff08720cd
Tatsiana Kastrubaiu <tatsiana.kastrubai@artocons.com>
2025-04-25 20:23:22 +0300

    refactor: add AuthenticationManager

--- src/main/java/com/artocons/carshop/security/config/SecurityConfig.java
+++ src/main/java/com/artocons/carshop/security/config/SecurityConfig.java
@@ -4,9 +4,11 @@ import com.artocons.carshop.security.user.ShopUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -22,6 +24,12 @@ public class SecurityConfig {
     private final ShopUserService shopUserService;
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return  authConfig.getAuthenticationManager();
+
+    }
+
+    @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(shopUserService);
@@ -35,7 +43,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .httpBasic(Customizer.withDefaults())
             .authorizeRequests((request) -> request
-                    .antMatchers("/admin/**").hasAuthority("ADMIN")
+                    .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
                     .anyRequest().permitAll()
             ).formLogin(form -> form
                         .loginPage("/auth")
